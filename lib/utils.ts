ebbf0571aac43a51edb298570c4cbdc39d8f7b21
Sony <jsaulhernandez98@gmail.com>
2024-12-03 11:38:54 -0600

    tag: adding utils to get data with converters from firebase

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,52 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { db } from "./firebase";
+import {
+  collection,
+  CollectionReference,
+  doc,
+  DocumentData,
+  DocumentReference,
+  QueryDocumentSnapshot,
+} from "firebase/firestore";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Define a generic converter function
+export function converter<T extends DocumentData>() {
+  return {
+    toFirestore: (data: T): DocumentData => {
+      return data;
+    },
+    fromFirestore: (snapshot: QueryDocumentSnapshot): T => {
+      const data = snapshot.data() as T;
+      return data;
+    },
+  };
+}
+
+export const dataPointCollection = <T extends DocumentData>(
+  collectionPath: string
+) => {
+  const collectionRef = collection(
+    db,
+    collectionPath
+  ) as CollectionReference<T>;
+
+  return collectionRef.withConverter(converter<T>());
+};
+
+export const dataPointDocument = <T extends DocumentData>(
+  documentPath: string,
+  ...pathSegments: string[]
+) => {
+  const fullPath = pathSegments
+    ? [documentPath, ...pathSegments].join("/")
+    : documentPath;
+  const docRef = doc(db, fullPath) as DocumentReference<T>;
+
+  return docRef.withConverter(converter<T>());
+};
